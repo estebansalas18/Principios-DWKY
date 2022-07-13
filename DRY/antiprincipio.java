19c8e670154bff46df1a83a79af775020422c133
Esteban Salas <75374934+estebansalas18@users.noreply.github.com>
2022-07-13 08:11:36 -0500

    Update antiprincipio.java

--- DRY/antiprincipio.java
+++ DRY/antiprincipio.java
@@ -9,4 +9,16 @@ public class antiprincipio {
             System.out.println("Generando reporte 1");
         }
     }
+    
+    public void generarReporte2() {
+        if(logeado = true && rol.equals("admin")) {
+            System.out.println("Generando reporte 2");
+        }
+    }
+    
+    public void generarReporte3() {
+        if(logeado = true && rol.equals("admin")) {
+            System.out.println("Generando reporte 3");
+        }
+    }
 }
